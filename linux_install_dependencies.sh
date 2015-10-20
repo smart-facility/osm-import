b89ecc3612ec5d5df199038f9a81b7ce1f501525
Matthew Berryman <mberryma@uow.edu.au>
2015-10-20 13:32:42 +1100

    Add script for installing linux dependencies.

---
+++ linux_install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run brew doctor. Identifies any issues that may cause following commands to fail.
+# Notably, the make install command relies on /usr/local ownership by local user
+sudo apt-get update
+sudo apt-get install osm2pgsql postgresql postgis postgresql-9.3-postgis-scripts git
+sudo make install
